c0ffc5c3264aab3d2c8f90015000ea553727c007
Cady Shelhon <cady.shelhon@aa.com>
2021-07-01 16:04:17 -0500

    Added tests for Car class

--- src/test/java/com/javapractice/CarTest.java
+++ src/test/java/com/javapractice/CarTest.java
@@ -7,15 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
 
-    @BeforeEach
-    void setUp() {
-    }
-
     @Test
     void getLicensePlateNumber() {
+        //given a car object with make "CHEVY" and licno "1234"
+        Car testCar = new Car("CHEVY", "1234");
+        //When getting License Plate number
+        String actual = testCar.getLicensePlateNumber();
+
+        //then returned value should match
+
+        assertEquals("1234", actual);
     }
 
     @Test
     void getFormattedInfo() {
+        //given a car object with make "CHEVY" and licno "1234"
+        Car testCar = new Car("CHEVY", "1234");
+        //When getFormattedInfo() is called, return "-CHEVY-1234"
+        String actual = testCar.getFormattedInfo();
+
+        //then returned value should match
+
+        assertEquals("-CHEVY-1234", actual);
     }
 }
